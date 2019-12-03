855bc2a575b7f9540c33e2cd80b8e81d1e77ed8d
Ravi Munnangi <ravimunnangi@Ravis-MacBook-Pro.local>
2019-12-02 22:43:34 -0600

    ex14

--- ex001.py
+++ ex001.py
@@ -29,3 +29,13 @@ print(list(my_range))
 print([i*10 for i in my_range])
 print([str(i) for i in my_range])
 print(list(map(str, my_range)))
+#Map, filter, reduce
+#Map - applies a function to all the items on the input_list - map(function, input_list)
+print(list(map(lambda x:str(x),my_range)))
+
+a = ["1", 1, "1", 2]
+print('list after removing duplicates - but order may not be preserved', list(set(a)))
+
+from collections import OrderedDict
+print('list after removing duplicates - order preserved', list(OrderedDict.fromkeys(a)))
+#python collections - read about them
